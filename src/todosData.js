886138b0930a7cd0ed36cb02ea207254734742a1
nithiskumar-r <nithiskumar2511@gmail.com>
2021-04-20 15:59:04 +0530

    First commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id:1,
+        text:"Place text 1",
+        completed:true
+    },
+    {
+        id:2,
+        text:"Place text 2",
+        completed:false
+    },
+    {
+        id:3,
+        text:"Place text 3",
+        completed:false
+    },
+    {
+        id:4,
+        text:"Place text 4",
+        completed:true
+    },
+    {
+        id:5,
+        text:"Place text 5",
+        completed:true
+    },
+]
+
+export default todosData
